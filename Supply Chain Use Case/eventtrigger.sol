27b0d6d5e389d0bb85dea4ca78805020c0b417c9
BharatXBhalla <81501575+BharatXBhalla@users.noreply.github.com>
2021-04-13 23:22:50 +0530

    Section 7 - Supply Truffle
    And Linked Item with Item Manager
    Supply Chain using Truffle Box React

--- Supply Chain Use Case/eventtrigger.sol
+++ Supply Chain Use Case/eventtrigger.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.4;
 
+contract Ownerable{
+    address public owner;
+    constructor(){
+        owner=msg.sender;
+    }
+    modifier onlyOwner(){
+        require(msg.sender == owner , "You are not Owner");
+        _;
+    }
+    function isOwner() public view returns(bool){
+        return(msg.sender == owner);
+    }
+}
 contract Item{
      uint public priceInWei;
      uint public pricePaid;
@@ -8,7 +21,7 @@ contract Item{
      
      ItemManager parentContract;
      
-     constructor(ItemManager _parentContract,uint _priceInWei,uint _index){
+     constructor(ItemManager _parentContract,uint _priceInWei,uint _index) {
          priceInWei=_priceInWei;
          index=_index;
          parentContract=_parentContract;
@@ -23,7 +36,7 @@ contract Item{
      }
 }
 
-contract ItemManager{
+contract ItemManager is Ownerable{
 
     enum SuppyChainState{Created,Paid,Delivered}
     
@@ -31,21 +44,22 @@ contract ItemManager{
         string _identifier;
         uint _itemPrice;
         ItemManager.SuppyChainState _state;
-        Item item;
+        Item _item;
     }
     
     mapping(uint => S_Item) public items;
     uint itemIndex;
     
-    event SuppyChainStep(uint _itemIndex, uint _step);
+    event SuppyChainStep(uint _itemIndex, uint _step,address _itemaddress);
     
-    function createItem(string memory _identifier,uint _itemPrice) public{
+    function createItem(string memory _identifier,uint _itemPrice) public onlyOwner{
         
+        Item item=new Item(this,_itemPrice,itemIndex);
         items[itemIndex]._identifier=_identifier;
         items[itemIndex]._itemPrice=_itemPrice;
         items[itemIndex]._state=ItemManager.SuppyChainState.Created;
-        
-        emit SuppyChainStep(itemIndex, uint(items[itemIndex]._state));
+        items[itemIndex]._item=item;
+        emit SuppyChainStep(itemIndex, uint(items[itemIndex]._state),address(item));
         
         itemIndex++;
     }
@@ -53,12 +67,12 @@ contract ItemManager{
         require(items[_itemIndex]._itemPrice == msg.value , "Only Full Payments Accepted");
         require(items[_itemIndex]._state == SuppyChainState.Created , "Item is Further in the chain");
         items[_itemIndex]._state =SuppyChainState.Paid;
-        emit SuppyChainStep(itemIndex, uint(items[itemIndex]._state));
+        emit SuppyChainStep(_itemIndex, uint(items[_itemIndex]._state),address(items[_itemIndex]._item));
     }
-    function triggerDelivery(uint _itemIndex) public{
+    function triggerDelivery(uint _itemIndex) public onlyOwner{
         require(items[_itemIndex]._state == SuppyChainState.Paid, "Not Paid ");
         items[_itemIndex]._state =SuppyChainState.Delivered;
-        emit SuppyChainStep(itemIndex, uint(items[itemIndex]._state));
+        emit SuppyChainStep(_itemIndex, uint(items[_itemIndex]._state),address(items[_itemIndex]._item));
         
     }
 }
